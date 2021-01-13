6b625b296cc6fa1884a8a75be3425959e9027d2e
Daniel Lopes Ferreira <daniel.aferreira@mercadolivre.com>
2021-01-13 18:32:55 -0300

    added config module

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+//example of using config file
+import config, { IConfig } from 'config'
+const dbConfig: IConfig = config.get('App.db')
+console.log(dbConfig.get('connectionString'))
+
 console.log('hello src')
