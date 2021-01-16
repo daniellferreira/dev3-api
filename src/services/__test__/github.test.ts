f965f8c234a97f000940a60da8687275bfa5d884
Daniel Lopes Ferreira <daniel.aferreira@mercadolivre.com>
2021-01-16 18:17:52 -0300

    change github client test

--- src/services/__test__/github.test.ts
+++ src/services/__test__/github.test.ts
@@ -2,20 +2,17 @@ import { GitHubClient, GitHubUserResponse } from '@src/clients/github'
 import { GitHubService } from '@src/services/github'
 import githubValidUserMock from '@test/mocks/github_valid_user.json'
 import githubNormalizedValidUserMock from '@test/mocks/github_normalized_valid_user.json'
-import { Request, Response } from '@src/lib/request'
 
-jest.mock('@src/lib/request')
+jest.mock('@src/clients/github')
 
 describe('GitHub service', () => {
-  const mockedRequest = new Request() as jest.Mocked<Request>
+  const mockedClient = new GitHubClient() as jest.Mocked<GitHubClient>
   it('should return the normalized user from GitHub', async () => {
-    mockedRequest.get.mockResolvedValue({
-      data: githubValidUserMock,
-    } as Response)
+    mockedClient.getUser.mockResolvedValue(
+      (githubValidUserMock as unknown) as GitHubUserResponse
+    )
 
-    const mockedClient = new GitHubClient(mockedRequest)
     const service = new GitHubService(mockedClient)
-
     const normalizedUser = await service.getUser('')
 
     expect(normalizedUser).toEqual(githubNormalizedValidUserMock)
